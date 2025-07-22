ecaa8fa51b3b28e79f4316b479f6a1e5cb24b4f3
ashutosh -singh <singhashu772@gmail.com>
2025-07-22 22:40:38 +0530

    third one

--- src/test/java/opencart/test/ExploreTutorialTest.java
+++ src/test/java/opencart/test/ExploreTutorialTest.java
@@ -1,5 +1,12 @@
 package opencart.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -20,8 +27,28 @@ public class ExploreTutorialTest extends BaseClass {
 				{ "Alice", "Smith", "alice.smith@example.com", "ANALYST", "Marketing: Advertising/Brand", "CodeInc", "IN" },
 				{ "Bob", "Brown", "bob.brown@example.com","SENIOR_VP", "IT: Security" ,"SoftSolutions", "IT" } };
 	}
+	
+	@DataProvider(name="userExcel")
+	public static Object[][] getDataFromExcel() throws IOException{
+		FileInputStream fis = new FileInputStream("D:\\SeleniumPractice\\OpenCart_Selenium\\test-data\\usersdetails.xlsx");
+		Workbook workbook = new XSSFWorkbook(fis);
+		Sheet sheet = workbook.getSheet("sheet1");
+		int RowCount = sheet.getLastRowNum();
+		int ColCount = sheet.getRow(0).getLastCellNum();
+		
+		Object[][] data = new Object[RowCount-1][ColCount];
+		for(int i=1; i<RowCount; i++) {
+			Row row = sheet.getRow(i);
+			for(int j=0; j<ColCount; j++) {
+				data[i-1][j]=row.getCell(j).toString();
+			}
+		}
+		workbook.close();
+		fis.close();
+		return data;
+	}
 
-	@Test(dataProvider = "userData")
+	@Test(dataProvider = "userExcel")
 	public void exploreTut(String firstName, String lastName, String email,
             String roleName, String department, String organisation, String country) {
 		exploreTutorials = new ExploreTutorials();
